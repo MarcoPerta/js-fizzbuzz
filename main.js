2a552f2e43f3a7f10502904fcc0c5b497733ea2c
MarcoPerta <perta.marco@gmail.com>
2022-10-21 15:54:41 +0200

    ciclo for ok, next step scrivere in html

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+
+let DivBox = document.getElementById('box');
 // Creo ciclo for
-for (let i=0; i < 100; i++){
-    if(i%3 = 0){
-        console.log('Buzz')
-    } if else (i%5 = 0) {
-        console.log('Fizz')
-    } if else ((i%3 = 0) && (i%5 = 0)){
+for (let i=1; i <= 100; i++){
+    if((i%3 === 0) && (i%5 === 0)){
         console.log('FizzBuzz')
+    } 
+    else if (i%5 === 0) {
+        console.log('Fizz')
+    } 
+    else if (i%3 === 0){
+        console.log('Buzz')
     }
-}
+    else {
+        console.log(i)
+    }
+
+    let stringa = '<p>il numero è $(`i`)</p>'
+    DivBox.innerHTML = stringa;
+} 
