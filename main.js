a00472fdf9e3ef25b7e5eba3491f8d3a3fc4718a
MarcoPerta <perta.marco@gmail.com>
2022-10-21 13:58:55 +0200

    creato ciclo for (non funziona)

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creo ciclo for
+for (let i=0; i < 100; i++){
+    if(i%3 = 0){
+        console.log('Buzz')
+    } if else (i%5 = 0) {
+        console.log('Fizz')
+    } if else ((i%3 = 0) && (i%5 = 0)){
+        console.log('FizzBuzz')
+    }
+}
