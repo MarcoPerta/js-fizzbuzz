9076446b8b4549207741c1fd2f2c46ba017e007e
MarcoPerta <perta.marco@gmail.com>
2022-10-22 16:28:00 +0200

    stampati html fizz buzz e numeri

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
+let divBox = document.getElementById('box')
 
-let divBox = document.getElementById('box');
-console.log(divBox);
+let divContainer = document.getElementById('container-fluid');
+console.log(divContainer);
 // Creo ciclo for
 for (let i=1; i <= 100; i++){
     if((i%3 === 0) && (i%5 === 0)){
+        let FizzBuzz = '<span>il numero è FizzBuzz</span>'
+        divContainer.innerHTML += FizzBuzz;
         console.log('FizzBuzz')
     } 
     else if (i%5 === 0) {
+        let Fizz = '<span>il numero è Fizz</span>'
+        divContainer.innerHTML += Fizz;
         console.log('Fizz')
     } 
     else if (i%3 === 0){
+        let Buzz = '<span>il numero è Buzz</span>'
+        divContainer.innerHTML += Buzz;
         console.log('Buzz')
     }
     else {
+        let stringa = `<span>il numero è ${i}</span>`
+        divContainer.innerHTML += stringa;
         console.log(i)
     }
 
-    let stringa = `<p>il numero è ${i}</p>`
-    divBox.innerHTML += stringa;
+    // let stringa = `<span>il numero è ${i}</span>`
+    // divContainer.innerHTML += stringa;
     
 } 
